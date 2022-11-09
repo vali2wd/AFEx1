17730acb8ff94bb8d673571f4497ee34a5f9b40c
vali2wd <95473852+vali2wd@users.noreply.github.com>
2022-11-09 22:22:09 +0200

    Modified from dictionary to sets.

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 class LabTwo:
 #solving first lab
     def __init__(self):
+        print("here")
         self.myFile = open("grafpond.in")
         self.vert, self.edges = self.findEV()
-        self.dic ={}
-        self.dic = self.readForm(self.dic)
+        self.graph = []
+        self.graph = self.readForm()
 
     def findEV(self):
         line = self.myFile.readline().split()
         return int(line[0]), int(line[1])
 
-
-    def readForm(self, myDict = {}):
-        myDict = {c:[] for c in range(1, self.vert + 1)}
+    def readForm(self):
+        r = []
         for i in range(self.edges):
             line = self.myFile.readline().split()
             a,b,c = int(line[0]), int(line[1]), int(line[2])
-            myDict[a].append([b,c])
+            r.append([a,b,c])
+            r.append([b,a,c])
+            r.sort(key= lambda x: x[2])
+        return r
 
-        return myDict
+    # def KruskalSlow(self):
+    #     self.dic.
 
 X = LabTwo()
-print(X.dic)
+print(X.graph)
