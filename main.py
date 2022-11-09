1e24ecf929a72dd7b564bfc623ffe08d7e1bdcc8
DESKTOP-I7HVH6A\Vali <vali_nafo@yahoo.com>
2022-11-10 00:22:30 +0200

    Added comments and fixed while edges

--- main.py
+++ main.py
@@ -21,20 +21,25 @@ class LabTwo:
         return r
 
     def find(self, parent, i):
+        # return recursively root of roots from quick-union
+        # https://www.cs.duke.edu/courses/cps100e/fall09/notes/UnionFind.pdf
         if parent[i] == i:
             return i
         return self.find(parent, parent[i])
 
     def union(self, parent, rank, x, y):
+        # weighted tree of quick-union
         xroot = self.find(parent, x)
         yroot = self.find(parent, y)
 
+        # attach smaller tree to bigger tree
         if rank[xroot] < rank[yroot]:
             parent[xroot] = yroot
         elif rank[xroot] > rank[yroot]:
             parent[yroot] = xroot
         else:
             parent[yroot] = xroot
+            # increment as to keep track of size
             rank[xroot] += 1
     def KruskalFast(self):
         output = []
@@ -42,16 +47,20 @@ class LabTwo:
         rank = []
         i, e = 0, 1
 
+        #initialize to fit nodes in array/ rank
         for node in range(self.vert + 1):
             parent.append(node)
             rank.append(0)
 
-        while e < self.vert-1:
+        #while for all nodes to be visited
+        while e < self.vert:
+            # picks edges in ascending order
             a, b, weight = self.graph[i]
             i += 1
             x = self.find(parent, a)
             y = self.find(parent, b)
 
+            # one way of saying: they won't form a cycle
             if x != y:
                 e += 1
                 output.append([a,b,weight])
@@ -61,10 +70,6 @@ class LabTwo:
             print(u, '-', v, '@', weight)
 
         return output
-
-
-
-
 
 
 X = LabTwo()
