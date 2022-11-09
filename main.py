5ac7e9be734343bea70da11407a83251cf8bf848
DESKTOP-I7HVH6A\Vali <vali_nafo@yahoo.com>
2022-11-10 00:10:12 +0200

    Completed Fast Kruskal.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 class LabTwo:
-#solving first lab
+    #solving first lab
     def __init__(self):
-        print("here")
         self.myFile = open("grafpond.in")
         self.vert, self.edges = self.findEV()
         self.graph = []
@@ -21,8 +20,52 @@ class LabTwo:
             r.sort(key= lambda x: x[2])
         return r
 
-    # def KruskalSlow(self):
-    #     self.dic.
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    def union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
+    def KruskalFast(self):
+        output = []
+        parent = []
+        rank = []
+        i, e = 0, 1
+
+        for node in range(self.vert + 1):
+            parent.append(node)
+            rank.append(0)
+
+        while e < self.vert-1:
+            a, b, weight = self.graph[i]
+            i += 1
+            x = self.find(parent, a)
+            y = self.find(parent, b)
+
+            if x != y:
+                e += 1
+                output.append([a,b,weight])
+                self.union(parent, rank, x, y)
+
+        for u, v, weight in output:
+            print(u, '-', v, '@', weight)
+
+        return output
+
+
+
+
+
 
 X = LabTwo()
-print(X.graph)
+X.KruskalFast()
