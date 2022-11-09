e529332333435e11fd5c01e744608319952692bb
vali2wd <95473852+vali2wd@users.noreply.github.com>
2022-11-09 18:25:36 +0200

    5th comment

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ class LabTwo:
 #sa bag un cod
 #si alt comment
 #third comment
+#fifth comment
     def __init__(self):
         self.myFile = open("grafpond.in")
         self.vert, self.edges = self.findEV()
